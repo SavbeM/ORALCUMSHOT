12ee3040c6df5a73562f81c482631758c88237d4
savbeM <mark.selihov2458@gmail.com>
2021-05-19 12:47:18 +0300

    react-practic

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from  'react';
+const Header = () => {
+      return (  <header className='header'>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/c/ce/Twitter_Logo.png"></img>
+        </header>
+    );
+}
+
+export default Header
